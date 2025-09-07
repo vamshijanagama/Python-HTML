b7c1d3a8ada3f7a8d76739b6f70c428015f41d89
Vamshi Janagama <vamshijanagama@Vamshis-MacBook-Air.local>
2025-09-07 02:27:40 -0500

    feat: add Day8 solutions (Python + HTML)

---
+++ Python/Day7_1_Finding the percentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    for i in student_marks:
+        if(i == query_name):
+            a = sum(student_marks[i])/3
+            print("{:.2f}".format(a))
